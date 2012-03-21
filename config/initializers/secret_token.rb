fa2c2523edbed2f171dc224db96a40297038fbb1
Alexey Nureev <navygator@myopera.com>
2012-03-21 08:41:42 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCalendar::Application.config.secret_token = '4a4af0b1be80b5da8c438be1f5c12900a5f90246515ba84a5a0a61b5f5df9807b2f03fcbef48c09168b1ea3caafbe279f1d280efa2bde823307ff6103ce1fc43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TestCalendar::Application.config.session_store :cookie_store, key: '_test_calendar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TestCalendar::Application.config.session_store :active_record_store
